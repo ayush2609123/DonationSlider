8741b03f3f3c1f2bd154e5db378780f276fcd2b8
ayush2609123 <112215034@cse.iiitp.ac.in>
2024-09-27 17:30:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CardSlider from './CardSlider.js';
+import './app.css'; // Ensure you import your CSS
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+
+    <div >
+       <div className='atitle'>
+      <h1>Your Help Can Make a Difference</h1>
+     </div>
+     <div>
+     <CardSlider/>
+    </div>
     </div>
   );
 }
